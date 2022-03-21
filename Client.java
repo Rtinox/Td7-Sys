dc533b6375b066fde66cc8e3d6d0415ec7659b80
Rtinox <nathanrouche30@gmail.com>
2022-03-21 23:28:48 +0000

    avancé td

--- Client.java
+++ Client.java
@@ -3,9 +3,29 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.Cipher;
 
 class Client {
+  static PrivateKey pvKey;
+  static PublicKey pbKey;
+  static PublicKey encodeKey;
+
   public static void main(String[] args) throws Exception {
+    KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+    keyPairGenerator.initialize(2048);
+    KeyPair keyPair = keyPairGenerator.genKeyPair();
+
+    pvKey = keyPair.getPrivate();
+    pbKey = keyPair.getPublic();
+
     String ip = (args.length == 1) ? args[0] : "localhost";
     int port = 6020;
     System.out.println("Connexion au serveur " + ip + ":" + port);
@@ -16,6 +36,14 @@ class Client {
     PrintWriter outs = new PrintWriter(new     OutputStreamWriter(socket.getOutputStream()), true);
     System.out.println("Entrez votre message (\"stop\" pour quitter):");
     
+    outs.println(pbKey.getEncoded().toString());
+    String keyString = ins.readLine();
+
+    X509EncodedKeySpec  keySpec = new X509EncodedKeySpec(keyString.getBytes());
+    KeyFactory keyFactory = KeyFactory.getInstance("RSA");	  
+    encodeKey = keyFactory.generatePublic(keySpec);
+    // TODO: Verifier la vérification de la clé
+
     // Read console input and send to sever
     String line;
     while ((line = new BufferedReader(new InputStreamReader(System.in)).readLine()) != null) {
@@ -28,5 +56,21 @@ class Client {
     ins.close();
     outs.close();
     socket.close();
+  }
+
+  public static byte[] encode(byte[] data, Key key) throws Exception {
+    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+    cipher.init(Cipher.ENCRYPT_MODE, key); 
+
+    byte[] bytes = cipher.doFinal(data);
+    return bytes;
+  }
+
+  public static byte[] decode(byte[] b, Key key) throws Exception {
+    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+    cipher.init(Cipher.DECRYPT_MODE, key); 
+
+    byte[] bytes = cipher.doFinal(b);
+    return bytes;
   }
 }
--- Serveur.java
+++ Serveur.java
@@ -5,13 +5,31 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.Cipher;
 
 
 class Serveur {
   
   static String STOP_MESSAGE = "stop";
+  static PrivateKey pvKey;
+  static PublicKey pbKey;
+  static PublicKey encodeKey;
   
   public static void main(String[] args) throws Exception {
+
+    KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+    keyPairGenerator.initialize(2048);
+    KeyPair keyPair = keyPairGenerator.genKeyPair();
+
+    pvKey = keyPair.getPrivate();
+    pbKey = keyPair.getPublic();
+
     ServerSocket serverSocket = new ServerSocket(6020);
     System.out.println("START");
     Socket socket = serverSocket.accept();
@@ -34,5 +52,21 @@ class Serveur {
     outs.close();
     socket.close();
     serverSocket.close();
+  }
+
+  public static byte[] encode(byte[] data, Key key) throws Exception {
+    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+    cipher.init(Cipher.ENCRYPT_MODE, key); 
+
+    byte[] bytes = cipher.doFinal(data);
+    return bytes;
+  }
+
+  public static byte[] decode(byte[] b, Key key) throws Exception {
+    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+    cipher.init(Cipher.DECRYPT_MODE, key); 
+
+    byte[] bytes = cipher.doFinal(b);
+    return bytes;
   }
 }
