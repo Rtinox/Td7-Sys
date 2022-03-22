39f36207cbadc646c1da709d1bd54004ec154ada
Rtinox <nathanrouche30@gmail.com>
2022-03-22 16:53:37 +0000

    ça fonctionne en client vers serveur

--- Client.java
+++ Client.java
@@ -4,28 +4,22 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.security.Key;
-import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+import javax.crypto.spec.SecretKeySpec;
 
 import javax.crypto.Cipher;
 
 class Client {
   static PrivateKey pvKey;
   static PublicKey pbKey;
-  static PublicKey encodeKey;
+  static Key key;
 
   public static void main(String[] args) throws Exception {
-    KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-    keyPairGenerator.initialize(2048);
-    KeyPair keyPair = keyPairGenerator.genKeyPair();
-
-    pvKey = keyPair.getPrivate();
-    pbKey = keyPair.getPublic();
-
     String ip = (args.length == 1) ? args[0] : "localhost";
     int port = 6020;
     System.out.println("Connexion au serveur " + ip + ":" + port);
@@ -33,22 +27,39 @@ class Client {
     Socket socket = new Socket(ip, port);
     BufferedReader ins = new BufferedReader(
     new InputStreamReader(socket.getInputStream()));
-    PrintWriter outs = new PrintWriter(new     OutputStreamWriter(socket.getOutputStream()), true);
+    PrintWriter outs = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+
+    System.out.println("Connecté, génération du couple des clé ...");
+    KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+    keyPairGenerator.initialize(2048);
+    KeyPair keyPair = keyPairGenerator.genKeyPair();
+
+    pvKey = keyPair.getPrivate();
+    pbKey = keyPair.getPublic();
+
+    System.out.println("Clés générés, envoi de celle-ci ...");
+    outs.println(Base64.getEncoder().encodeToString(pbKey.getEncoded()));
+
+    System.out.println("Récupération de la clé DES crypté ...");
+    String DESEncodedKey = ins.readLine();
+
+    System.out.println("Décriptage de la clé ...");
+    byte[] decodedDES = Base64.getDecoder().decode(DESEncodedKey.getBytes());
+    decodedDES = decode(decodedDES, pvKey, "RSA");
+    key = new SecretKeySpec(decodedDES, 0, decodedDES.length, "DES");
+    
     System.out.println("Entrez votre message (\"stop\" pour quitter):");
     
-    outs.println(pbKey.getEncoded().toString());
-    String keyString = ins.readLine();
-
-    X509EncodedKeySpec  keySpec = new X509EncodedKeySpec(keyString.getBytes());
-    KeyFactory keyFactory = KeyFactory.getInstance("RSA");	  
-    encodeKey = keyFactory.generatePublic(keySpec);
-    // TODO: Verifier la vérification de la clé
-
-    // Read console input and send to sever
     String line;
     while ((line = new BufferedReader(new InputStreamReader(System.in)).readLine()) != null) {
-      outs.println(line);
-      System.out.println("Server: " + ins.readLine());
+      byte[] encoded_message = encode(line.getBytes(), key, "DES");
+      String base64_message = Base64.getEncoder().encodeToString(encoded_message);
+      outs.println(base64_message);
+
+      String serveur_message = ins.readLine();
+      byte[] base64_decode = Base64.getDecoder().decode(serveur_message);
+      byte[] decoded = decode(base64_decode, key, "DES");
+      System.out.println("Server: " + new String(decoded));
       if (line.equals("stop")) break;
     }
     
@@ -58,17 +69,17 @@ class Client {
     socket.close();
   }
 
-  public static byte[] encode(byte[] data, Key key) throws Exception {
-    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-    cipher.init(Cipher.ENCRYPT_MODE, key); 
+  public static byte[] encode(byte[] data, Key key, String mode) throws Exception {
+    Cipher cipher = Cipher.getInstance(mode);
+    cipher.init(Cipher.ENCRYPT_MODE, key);
 
     byte[] bytes = cipher.doFinal(data);
     return bytes;
   }
 
-  public static byte[] decode(byte[] b, Key key) throws Exception {
-    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-    cipher.init(Cipher.DECRYPT_MODE, key); 
+  public static byte[] decode(byte[] b, Key key, String mode) throws Exception {
+    Cipher cipher = Cipher.getInstance(mode);
+    cipher.init(Cipher.DECRYPT_MODE, key);
 
     byte[] bytes = cipher.doFinal(b);
     return bytes;
--- Serveur.java
+++ Serveur.java
@@ -6,65 +6,78 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.Key;
-import java.security.KeyPair;
+import java.security.KeyFactory;
 import java.security.KeyPairGenerator;
-import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 import javax.crypto.Cipher;
-
+import javax.crypto.KeyGenerator;
 
 class Serveur {
-  
+
   static String STOP_MESSAGE = "stop";
-  static PrivateKey pvKey;
-  static PublicKey pbKey;
-  static PublicKey encodeKey;
-  
+  static PublicKey pbKey = null;
+  static Key desKey;
+
   public static void main(String[] args) throws Exception {
-
-    KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-    keyPairGenerator.initialize(2048);
-    KeyPair keyPair = keyPairGenerator.genKeyPair();
-
-    pvKey = keyPair.getPrivate();
-    pbKey = keyPair.getPublic();
-
     ServerSocket serverSocket = new ServerSocket(6020);
     System.out.println("START");
     Socket socket = serverSocket.accept();
     BufferedReader ins = new BufferedReader(
-      new InputStreamReader(socket.getInputStream()));
-    
+        new InputStreamReader(socket.getInputStream()));
+
     PrintWriter outs = new PrintWriter(new BufferedWriter(
-      new OutputStreamWriter(socket.getOutputStream())), true);
-    
-    
+        new OutputStreamWriter(socket.getOutputStream())), true);
+
     while (true) {
       String line = ins.readLine();
-      if(line == null) continue;
-      System.out.println("Reçu: " + line);
-      outs.println(line);
-      if(line.equals(STOP_MESSAGE)) break;
+      if (line == null)
+        continue;
+      if (pbKey == null) {
+        System.out.println("Récupération de la clé publique RSA");
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(line));
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        pbKey = keyFactory.generatePublic(keySpec);
+
+        // 2 . Création de la clé DES
+        System.out.println("Génération de la clé DES");
+        KeyGenerator keyGen = KeyGenerator.getInstance("DES");
+        keyGen.init(56);
+        desKey = keyGen.generateKey();
+        byte[] keyB = desKey.getEncoded();
+        byte[] desEncoded = encode(keyB, (Key) pbKey, "RSA");
+
+        System.out.println("Envoie de la clé DES");
+        outs.println(Base64.getEncoder().encodeToString(desEncoded));
+        continue;
+      }
+      byte[] message = decode(Base64.getDecoder().decode(line.getBytes()), desKey, "DES");
+      String message_decoded = new String(message);
+      System.out.println("Reçu: " + message_decoded);
+      outs.println(Base64.getEncoder().encodeToString(encode(message_decoded.getBytes(), desKey, "DES")));
+      if (new String(message).equals(STOP_MESSAGE))
+        break;
     }
-    
+
     ins.close();
     outs.close();
     socket.close();
     serverSocket.close();
   }
 
-  public static byte[] encode(byte[] data, Key key) throws Exception {
-    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-    cipher.init(Cipher.ENCRYPT_MODE, key); 
+  public static byte[] encode(byte[] data, Key key, String mode) throws Exception {
+    Cipher cipher = Cipher.getInstance(mode);
+    cipher.init(Cipher.ENCRYPT_MODE, key);
 
     byte[] bytes = cipher.doFinal(data);
     return bytes;
   }
 
-  public static byte[] decode(byte[] b, Key key) throws Exception {
-    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-    cipher.init(Cipher.DECRYPT_MODE, key); 
+  public static byte[] decode(byte[] b, Key key, String mode) throws Exception {
+    Cipher cipher = Cipher.getInstance(mode);
+    cipher.init(Cipher.DECRYPT_MODE, key);
 
     byte[] bytes = cipher.doFinal(b);
     return bytes;
