0677456ea5a25fdc06df2b9b536323566babe110
Rtinox <nathanrouche30@gmail.com>
2022-03-23 22:28:32 +0000

    end ?

--- Serveur.java
+++ Serveur.java
@@ -18,14 +18,14 @@ class Serveur {
     System.out.println("Serveur démarré, en attente d'un client ...");
 
     // Création d'un thread permettant d'envoyer des messages depuis le serveur vers
-    // le client
+    // les clients
     Thread t = new Thread() {
       public void run() {
         try {
           String line;
           while ((line = new BufferedReader(new InputStreamReader(System.in)).readLine()) != null) { // Lecture de
                                                                                                      // l'entrée clavier
-            sendToAll("[Server] " + line);
+            sendToAll("[Server] " + line); // Envoi à tous les clients
             if (line.equals("stop"))
               break;
           }
@@ -37,22 +37,24 @@ class Serveur {
     t.start();
 
     while (true) {
-      Socket s = serverSocket.accept();
-      Connection con = new Connection(s);
-      executor.execute(con);
-      connections.add(con);
+      Socket s = serverSocket.accept(); // Accepte la connexion du client
+      Connection con = new Connection(s); // Creer un thread de la classe Connection
+      executor.execute(con); // Execute ce thread
+      connections.add(con); // Ajout à la liste des differentes connexions
     }
 
     // Fermeture de la sockets
     // serverSocket.close();
   }
 
+  // Envoi d'un message à tous les clients
   public static void sendToAll(String message) {
     for (int i = 0; i < connections.size(); i++) {
       connections.get(i).send(message);
     }
   }
 
+  // Supression d'une connection de la liste
   public static void removeConnection(Connection connection) {
     connections.remove(connection);
   }
